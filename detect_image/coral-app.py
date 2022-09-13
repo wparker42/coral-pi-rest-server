c6454c5b52f8f04d9f99867298a585c98c781493
Ty Parker <wparker42@users.noreply.github.com>
2022-09-12 18:03:50 -0600

    Moved model loading from call to init

--- detect_image/coral-app.py
+++ detect_image/coral-app.py
@@ -1,28 +1,41 @@
-# Start the server:
-#   python3 coral-app.py
-# Submit a request via cURL:
-#   curl -X POST -F image=@images/test-image3.jpg 'http://localhost:5000/v1/vision/detection'
-#   curl -X POST -F image=@images/test-image3.jpg 'http://raspi.local:5000/v1/vision/detection'
+"""Perform inference using tensorflow-lite deep learning models with hardware
+acceleration provided by a Coral usb accelerator running on a raspberry pi or
+linux/mac.
+
+The models are exposed via a REST API allowing inference over a network.
+
+    Typical usage example:
+
+    Start the server:
+        python3 coral-app.py
+    Submit a request via cURL:
+        curl -X POST -F image=@images/test-image3.jpg \
+            'http://localhost:5000/v1/vision/detection'
+        curl -X POST -F image=@images/test-image3.jpg \
+            'http://raspi.local:5000/v1/vision/detection'
+
+"""
+
 
 import argparse
 import os
 import time
-import pathlib
 import logging
 import flask
-import numpy as np
 from PIL import Image, ImageDraw
-from pycoral.adapters import classify, detect, common
+from pycoral.adapters import detect, common
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.dataset import read_label_file
 
 app = flask.Flask(__name__)
 
 LOGFORMAT = "%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s"
-logging.basicConfig(filename="coral.log", level=logging.DEBUG, format=LOGFORMAT)
+logging.basicConfig(filename="../coral.log", level=logging.DEBUG,
+                    format=LOGFORMAT)
 stderrLogger=logging.StreamHandler()
 stderrLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
 logging.getLogger().addHandler(stderrLogger)
+logging.getLogger('PIL').setLevel(logging.WARNING)
 
 
 DEFAULT_MODELS_DIRECTORY = "../models"
@@ -32,10 +45,10 @@ ROOT_URL = "/v1/vision/detection"
 DRAW_URL = "/v1/vision/processing"
 IMAGE_SAVE_PATH = "../saved_images/output.jpg"
 IMAGE_SAVE_QUALITY = 95
-COUNT = 5
+COUNT = 5  # number of time the inferrence is run
 
 
-def draw_objects(image, objs, labels):
+def draw_objects(image, objs, lbls):
     """Draws the bounding box and label for each object."""
     draw = ImageDraw.Draw(image)
     for obj in objs:
@@ -43,14 +56,14 @@ def draw_objects(image, objs, labels):
         draw.rectangle([(bbox.xmin, bbox.ymin), (bbox.xmax, bbox.ymax)],
                        outline='red')
         draw.text((bbox.xmin + 10, bbox.ymin + 10),
-                  '%s\n%.2f' % (labels.get(obj.id, obj.id), obj.score),
+                  '%s\n%.2f' % (lbls.get(obj.id, obj.id), obj.score),
                   fill='red')
     image.save(IMAGE_SAVE_PATH, quality=IMAGE_SAVE_QUALITY)
-    return None
+    # return None
 
 @app.route("/")
 def info():
-    info_str = "Flask app exposing tensorflow lite model {}".format(model_file)
+    info_str = f"Flask app exposing tensorflow lite model {model_file}"
     return info_str
 
 @app.route(DRAW_URL, methods=['POST', 'GET'])
@@ -62,66 +75,54 @@ def send_image():
 def predict():
     data = {"success": False}
 
-    if flask.request.method == "POST":
-        print('\n ------- POST --------')
 
-        if flask.request.files.get("image"):
-            # Init the interpreter
-            print("\n Initialising interpreter")
-            interpreter = make_interpreter(model_file)
-            print("\n Initialised interpreter with model : {}".format(model_file))
-            interpreter.allocate_tensors()
-            print("\n Allocated tensors")
+    if flask.request.files.get("image"):
+        # Process the image
+        image_file = flask.request.files["image"]
+        image = Image.open(image_file)
+        # image = Image.open("../images/test-image3.jpg")
+        _, scale = common.set_resized_input(
+            interpreter,
+            image.size,
+            lambda s: image.resize(s, Image.ANTIALIAS))
 
-            # Process the image
-            image_file = flask.request.files["image"]
-            image = Image.open(image_file)
-            # image = Image.open("../images/people_car.jpg")
-            # Transform image if required
-            size = common.input_size(interpreter)
-            params = common.input_details(interpreter, 'quantization_parameters')
-            _, scale = common.set_resized_input(
-                interpreter,
-                image.size,
-                lambda size: image.resize(size, Image.ANTIALIAS))
-           
-            # Run the inference
-            if flask.request.args.get("thresh"):
-                score_min = float(flask.request.args.get("thresh"))
-            else:
-                score_min = 0.45
-            print(f'\n Score threshold: {score_min}')
-            print('\n ---- INFERENCE TIME ----')
-            print('\n Note: The first inference on Edge TPU is slow because it includes',
-            'loading the model into Edge TPU memory.')
-            for _ in range(COUNT):
-                start = time.perf_counter()
-                interpreter.invoke()
-                inference_time = time.perf_counter() - start
-                objs = detect.get_objects(interpreter, score_min, scale)
-                print('%.1fms' % (inference_time * 1000))
-            data["duration"] = inference_time
+        # Run the inference
+        if flask.request.args.get("thresh"):
+            score_min = float(flask.request.args.get("thresh"))
+        else:
+            score_min = 0.45
+        print(f'\n Score threshold: {score_min}')
+        print('\n ------ INFERENCE TIME ------')
+        print('\n Note: The first inference on Edge TPU is slow because it includes',
+        'loading the model into Edge TPU memory.')
+        for _ in range(COUNT):
+            start = time.perf_counter()
+            interpreter.invoke()
+            inference_time = time.perf_counter() - start
+            objs = detect.get_objects(interpreter, score_min, scale)
+            print('%.1fms' % (inference_time * 1000))
+        data["duration"] = round(inference_time, 5)
 
-            # Process the results
-            print('\n ------- RESULTS --------')
-            if objs:
-                inferd_objects = []
-                for obj in objs:
-                    print('%s: %.5f' % (labels.get(obj.id, obj.id), obj.score))
-                    inferd_objects.append(
-                        {
-                            "confidence": float(obj.score),
-                            "label": labels[obj.id],
-                            "y_min": int(obj.bbox[1]),
-                            "x_min": int(obj.bbox[0]),
-                            "y_max": int(obj.bbox[3]),
-                            "x_max": int(obj.bbox[2]),
-                        })
-                data["predictions"] = inferd_objects
-                draw_objects(image, objs, labels)
-            else:
-                print('\n No objects detected')
-                data["predictions"] = []
+        # Process the results
+        print('\n ------- RESULTS --------')
+        if objs:
+            inferd_objects = []
+            for obj in objs:
+                print('%s: %.5f' % (labels.get(obj.id, obj.id), obj.score))
+                inferd_objects.append(
+                    {
+                        "confidence": float(obj.score),
+                        "label": labels[obj.id],
+                        "y_min": int(obj.bbox[1]),
+                        "x_min": int(obj.bbox[0]),
+                        "y_max": int(obj.bbox[3]),
+                        "x_max": int(obj.bbox[2]),
+                    })
+            data["predictions"] = inferd_objects
+            draw_objects(image, objs, labels)
+        else:
+            print('\n No objects detected')
+            data["predictions"] = []
 
     # return the data dictionary as a JSON response
     data["success"] = True
@@ -144,15 +145,18 @@ if __name__ == '__main__':
                         help="port number")
     args = parser.parse_args()
 
-    global model_file
     model_file = os.path.join(args.models_directory, args.model)
     assert os.path.isfile(model_file)
     print(f'\n Using tensorflow lite model: {model_file}')
 
     labels_file = os.path.join(args.models_directory, args.labels)
     assert os.path.isfile(labels_file)
-    global labels
     labels = read_label_file(labels_file)
     print(f'\n Using labels: {labels_file}\n')
 
-    app.run(host="0.0.0.0", debug=True, port=args.port)
+    interpreter = make_interpreter(model_file)
+    print(f"\n Initialised interpreter with model : {model_file}")
+    interpreter.allocate_tensors()
+    print("\n Allocated tensors")
+
+    app.run(host="0.0.0.0", debug=True, port=args.port, use_reloader=False)
